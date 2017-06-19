d82b69fee96b9dd80b5a9b83e5cf5169cffdfd78
Pansera Oktasedu <pansedo@gmail.com>
2017-06-19 13:23:32 +0700

    Update page LMS

--- setting/controller/mapel.php
+++ setting/controller/mapel.php
@@ -5,9 +5,9 @@ class Mapel
     public function __construct() {
         try {
             global $db;
-            $tableName = 'mata_pelajaran';
-            $this->db = $db;
-            $this->db->table = $this->db->$tableName;
+            $tableName  = 'mata_pelajaran';
+            $this->db   = $db;
+            $this->table = $this->db->$tableName;
         } catch(Exception $e) {
             echo "Database Not Connection";
             exit();
@@ -16,7 +16,7 @@ class Mapel
 
     public function getInfoMapel($idMapel){
 		$ID     = new MongoId($idMapel);
-        $query  = $this->db->mata_pelajaran->findOne(array("_id" => $ID));
+        $query  = $this->table->findOne(array("_id" => $ID));
 		if($query['_id']){
 			$query1	= $this->db->modul->find(array("id_mapel" => $idMapel))->count();
 			$query['modul'] = $query1;
@@ -25,14 +25,14 @@ class Mapel
     }
 
     public function getListbyKelas($idkelas){
-        $query =  $this->db->mata_pelajaran->find(array("id_kelas"=>"$idkelas"));
+        $query =  $this->table->find(array("id_kelas"=>"$idkelas"));
         return $query;
     }
 
     public function addMapel($nama, $kelas, $user){
         $newID  = "";
         $insert = array("id_kelas"=>$kelas, "nama" => $nama, "creator" => "$user", "date_created"=>date('Y-m-d H:i:s'), "date_modified"=>date('Y-m-d H:i:s'));
-                  $this->db->mata_pelajaran->insert($insert);
+                  $this->table->insert($insert);
         $newID  = $insert['_id'];
         if ($newID) {
             $status     = "Success";
@@ -43,6 +43,24 @@ class Mapel
         }
 
         $result = array("status" => $status, "message"=>$message, "IDMapel" => $newID);
+        return $result;
+    }
+
+    public function updateMapel($nama, $mapel){
+        $update     = array('$set' => array("nama"=>$nama, "date_modified"=>date('Y-m-d H:i:s')));
+
+          try {
+              $this->table->update(array("_id" => new MongoId($mapel)), $update);
+              $status   = "success";
+              $judul    = "Berhasil!";
+              $message  = "Pengaturan Mata Pelajaran berhasil disimpan.";
+          } catch(MongoCursorException $e) {
+              $status   = "error";
+              $judul    = "Maaf!";
+              $message  = "Pengaturan Mata Pelajaran gagal disimpan.";
+          }
+
+        $result = array("status" => $status, "judul" => $judul, "message"=>$message, "IDMapel" => $mapel);
         return $result;
     }
 
